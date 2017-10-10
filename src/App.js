d445fefc3f071fa7e3806c8ff42658157c0f5edc
Eric Schwartz <eric.schwartz@theironyard.com>
2017-10-10 11:46:52 -0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Character from './containers/Character'
 import Navigation from './components/navigation/Navigation'
 
 class App extends Component {
   render() {
     return (
       <div>
-        eric was here
         <Navigation />
-        <Character />
+        {this.props.children}
       </div>
     );
   }
